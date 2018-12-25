16dd5ce130e6ff22b51f1b2fede8ab7fa520003c
Chris Athanas <realityexpander@gmail.com>
2018-12-25 01:00:06 -0600

    Add time stamp polling

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', function (socket) {
+  console.log('user connected');
+  socket.on('chat message', function (msg) {
     io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
   });
 });
 
-http.listen(port, function(){
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
+
+setInterval(function () {
+  io.emit('chat message', Date())
+}, 5000)
