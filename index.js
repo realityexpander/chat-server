009d8cbf6b0261328caaaefa4db5b5c2d7eface1
Chris Athanas <realityexpander@gmail.com>
2018-12-25 17:38:12 -0600

    Allow remote async data calls

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
   console.log('user connected', socket.client.id);
+  io.emit('chat message', 'User connected: ' + socket.client.id);
   socket.on('chat message', function (msg) {
     console.log("from:", this.conn.id, ":", msg)
-    io.emit('chat message', msg);
+    io.emit('chat message', this.conn.id + " -> " + msg);
   });
   socket.on('disconnect', function () {
-    console.log('user disconnected', arguments);
+    io.emit('chat message', 'User disconnected: ' + this.conn.id);
+    console.log('user disconnected', this.conn.id);
   });
 });
 
@@ -23,5 +25,21 @@ http.listen(port, function () {
 });
 
 setInterval(function () {
-  io.emit('chat message', Date() + " => " + Math.trunc(Math.random() * 500))
-}, 5000)
+  io.emit('chat message', Date())
+}, 10000)
+
+
+
+// Try these remote evals
+// eval! $("form").append($("<p>").text("hi").css("background", "yellow"))
+// eval! $("button").css("background","red")
+// eval! socket.emit('chat message', JSON.stringify(Date()));
+// eval! socket.emit('chat message', new Date().getMilliseconds())
+
+// eval!$.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
+//      socket.emit('chat message', data.geobytesipaddress) 
+//     })
+
+// eval!$.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
+//      socket.emit('chat message', JSON.stringify(data)) 
+//     })
