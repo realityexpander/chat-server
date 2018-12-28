b139658288e13141ff529eb8d0844cb230890a9f
Chris Athanas <realityexpander@gmail.com>
2018-12-28 15:04:48 -0600

    Add ability to trigger debugger remotely

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ io.on('connection', function (socket) {
   io.emit('chat message', 'User connected: ' + socket.client.id);
   socket.on('chat message', function (msg) {
     console.log("from:", this.conn.id, ":", msg)
-    io.emit('chat message', this.conn.id + " -> " + msg);
+
+    if (('' + msg).indexOf('debugger') >= 0)
+      debugger;
+
+    io.emit('chat message', this.conn.id.slice(-4).split("").reverse().join("") + " -> " + msg);
   });
   socket.on('disconnect', function () {
     io.emit('chat message', 'User disconnected: ' + this.conn.id);
@@ -29,7 +33,6 @@ setInterval(function () {
 }, 10000)
 
 
-
 // Try these remote evals
 // eval! $("form").append($("<p>").text("hi").css("background", "yellow"))
 // eval! $("button").css("background","red")
@@ -43,3 +46,33 @@ setInterval(function () {
 // eval!$.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
 //      socket.emit('chat message', JSON.stringify(data)) 
 //     })
+
+// add to messages
+// eval!$('<img src="'+ 'https://i.ytimg.com/vi/PxG0cprSWcU/hqdefault.jpg' 
+//  +'"></li>').load(function() {   $(this).width(100).height(100).appendTo('#messages'); });
+
+// eval!setInterval(() => {
+//   let tmp = new Date().getMilliseconds();
+//   socket.emit('chat message', tmp - window.__lastTime);
+//   window.__lastTime = tmp;
+// }, 2000)
+
+// to see debugger remotely
+// chrome://inspect/#devices
+// add target discovery settings:
+//   realityexpander.ml:3001
+
+// add to body
+// eval!$('<img src="'+ 'https://i.ytimg.com/vi/PxG0cprSWcU/hqdefault.jpg'   +'"></li>')
+//   .load(function() {   $(this).width(100).height(100)   
+//   .css({top: '100px', left: '100px', position:'absolute'}).appendTo('#messages'); });
+
+// eval!
+// [10,20,30,40,50,60].map( i=> {
+//   $('<img src="'+ 'https://i.ytimg.com/vi/PxG0cprSWcU/hqdefault.jpg'   +'"></li>')
+//    .load(function() {   
+//     $(this).width(i*10).height(i*10)   
+//      .css({top: i*20+'px', left: i*20+'px', position:'absolute'})
+//      .appendTo('#messages'); 
+//     }); 
+//   })
